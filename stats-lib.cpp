00d1a234619d70997cfe3295f3fc8f3925e694c5
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2023-01-13 21:39:40 +0530

    2023.01.13

--- stats-lib.cpp
+++ stats-lib.cpp
@@ -1,10 +1,8 @@
 /*
-    version-2023.01.11
+    version-2023.01.13
     Created by Sahil Rajwar
     Date: 11th January 2023
-    HomePage: https://www.github.com/Sahil-Rajwar-2004/statslib-cpp
-
-    
+    HomePage: https://www.github.com/Sahil-Rajwar-2004/stats-lib-cpp
 */
 
 #include <iostream>
@@ -14,6 +12,7 @@
 #include <cmath>
 #include <numeric>
 #include <string>
+#include <climits>
 using namespace std;
 
 
@@ -28,8 +27,63 @@ string author = "Sahil Rajwar";
 string version = "2023.01.11";
 string homepage = "https://github.com/Sahil-Rajwar-2004/stats-lib";
 
+int error = -1;
+
+class Mathematics{
+    public:
+        int factorial(int num){
+            if(num == 0){
+                return 1;
+            }else if(num < 0){
+                return error;
+            }
+            return num*factorial(num-1);
+        }
+
+        int permutation(int n,int r){
+            float res = factorial(n)/factorial(n-r);
+            return res;
+        }
+
+        int combination(int n,int r){
+            float res = factorial(n)/(factorial(r)*factorial(n-r));
+            return res;
+        }
+
+        int abs(float n){
+            if(n<0){
+                return -n;
+            }else if(n == 0){
+                return 0;
+            }else{
+                return n;
+            }
+        }
+};
+
+
 class Statistics{
     public:
+        double max(vector<double> &data){
+            double max = INT_MIN;
+            for(int i=0;i<data.size();i++){
+                if(max<data[i]){
+                    max = data[i];
+                }
+            }
+            return max;
+        }
+
+        double min(vector<double> &data){
+            double min = INT_MAX;
+            for(int i=0;i<data.size();i++){
+                if(min>data[i]){
+                    min = data[i];
+                }
+            }
+            return min;
+        }
+
 		double sum(vector<double> &data){
 			float x = 0.0;
 			for(int i=0;i<data.size();i++){
@@ -107,7 +161,7 @@ class Statistics{
 				    float a = 1/data[i];
 				    sum += a;
                 }else{
-                    return -1;
+                    return error;
                 }
 			}
 			return data.size()/sum;
@@ -128,12 +182,75 @@ class Statistics{
             return sum;
         }
 
-        double standard_deviation(vector<double> &data){
+        double sample_standard_deviation(vector<double> &data){
             vector<double> res;
             for(int i=0;i<len(data);i++){
                 float x = pow(data[i]-mean(data),2);
                 res.push_back(x);
             }
             return sqrt(sum(res)/(len(data)-1));
+        }
+
+        double population_standard_deviation(vector<double> &data){
+            vector<double> res;
+            for(int i=0;i<data.size();i++){
+                float a = pow(data[i]-mean(data),2);
+                res.push_back(a);
+            }
+            return sum(res)/len(data);
+        }
+
+        double sample_variance(vector<double> &data){
+            vector<double> res;
+            for(int i=0;i<len(data);i++){
+                float a = pow(data[i]-mean(data),2);
+                res.push_back(a);
+            }
+            return sum(res)/(len(data)-1);
+        }
+
+        double population_variance(vector<double> &data){
+            vector<double> res;
+            for(int i=0;i<data.size();i++){
+                float a = pow(data[i]-mean(data),2);
+                res.push_back(a);
+            }
+            return sum(res)/len(data);
+        }
+
+        double sample_standard_error(vector<double> &data){
+            return sample_standard_deviation(data)/sqrt(len(data));
+        }
+
+        double population_standard_error(vector<double> &data){
+            return population_standard_deviation(data)/sqrt(len(data));
+        }
+
+        double rms(vector<double> &data){
+            vector<double> res;
+            for(int i=0;i<data.size();i++){
+                float a = pow(data[i],2);
+                res.push_back(a);
+            }
+            return sqrt(sum(res)/len(data));
+        }
+
+        double mse(vector<double> &actual,vector<double> &predicted){
+            if(len(actual)==len(predicted)){
+                vector<double> errors;
+                for(int i=0;i<len(actual);i++){
+                    float a = pow(actual[i]-predicted[i],2);
+                    errors.push_back(a);
+                }
+                return sum(errors)/len(actual);
+            }
+            return error;
+        }
+
+        double rmse(vector<double> &actual, vector<double> &predicted){
+            if(len(actual)==len(predicted)){
+                return sqrt(mse(actual,predicted));
+            }
+            return error;
         }
 };
